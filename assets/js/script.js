2fb5011a247dbb1768257ee8c59d155b4014ed86
Kevin Patto <kevp2010@gmail.com>
2023-09-26 21:23:15 -0500

    created variables, added a start button & enter score screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
+// variables spaced out by section
 var viewScoresEl = document.querySelector("#hi-score-span");
+var timerEl = document.querySelector("#timer-span");
+
+var startQuizSectionEl = document.querySelector("#start-quiz");
+var startQuizBtnEl = document.querySelector("#start-btn");
+
+var quizSectionEl = document.querySelector("#quiz");
+var questionTextEl = document.querySelector("#question");
+var answerBtnsEl = [
+    document.querySelector("#a1"),
+    document.querySelector("#a2"),
+    document.querySelector("#a3"),
+    document.querySelector("#a4")
+];
+
+var enterScoreSectionEl = document.querySelector("#enter-score");
+var achievedScoreEl = document.querySelector("#achieved-score-span");
+var inputEl = document.querySelector("#initials-input");
+var submitBtnEl = document.querySelector("#submit-btn");
+
+var highscoresSectionEl = document.querySelector("#highscores");
+var scoresLiEl = document.querySelector("#scores");
+
